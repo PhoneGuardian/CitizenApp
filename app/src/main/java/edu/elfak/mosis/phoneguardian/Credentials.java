2a860b107d0b74357b4dc730807c5762e7ea92bd
nemtajo <nemtajo@yahoo.com>
2014-12-16 15:39:49 +0100

    dodato cuvanje kredencijala na sd karticu

---
+++ app/src/main/java/edu/elfak/mosis/phoneguardian/Credentials.java
@@ -0,0 +1,26 @@
+package edu.elfak.mosis.phoneguardian;
+
+/**
+ * Created by Nemanja on 16/12/2014.
+ */
+
+public class Credentials {
+    private String Username;
+    private String PhoneNumber;
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+}
