76fe0db4517d3f49e190724256beaac691a8ee73
milica13629 <kikice010@gmail.com>
2014-12-16 15:45:28 +0100

    Deleted the alarm and upload photo option in AddLocationActivity

--- app/src/main/java/edu/elfak/mosis/phoneguardian/AddLocationActivity.java
+++ app/src/main/java/edu/elfak/mosis/phoneguardian/AddLocationActivity.java
@@ -56,10 +56,6 @@ public class AddLocationActivity extends Activity implements OnClickListener {
 	
 	String category = "Red zone!";
 	boolean alertClicked = false;
-	int brojac;
-	
-	String photo_dest; //destination of photo in phone
-	Uri selectedImage; 
 	
 	static final int REQUEST_IMAGE_CAPTURE = 1;
 	final String TAG_SUCCESS = "success";
@@ -80,104 +76,44 @@ public class AddLocationActivity extends Activity implements OnClickListener {
 	File photo;
 	int serverResponseCode = 0;
 	ProgressDialog dialog = null;
-	
-	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+
+
 	Calendar cal = Calendar.getInstance();
-	
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.addlocation_activity);
-		
-		brojac=0;
-		new AlertPlayer().execute();
-		
+
 		user = (TextView) findViewById(R.id.label_username);
 		time = (TextView) findViewById(R.id.label_addingtime);
 		longitude = (TextView) findViewById(R.id.label_long);
 		latitude = (TextView) findViewById(R.id.label_lat);
 		address = (TextView) findViewById(R.id.label_address);
 		description = (EditText) findViewById(R.id.edit_text_descr);
-		Button btnTakePic = (Button) findViewById(R.id.btn_take_picture);
+
 		Button btnSave = (Button) findViewById(R.id.btn_save_location);
-		
-		
-		btnTakePic.setOnClickListener(this);
+
 		btnSave.setOnClickListener(this);
-		
+
 		Calendar cal = Calendar.getInstance();
 		time.setText(dateFormat.format(cal.getTime()));
-		
-		
+
+
 		Location location = getlocation();
 		latitude.setText(Double.toString(location.getLatitude()));
 		longitude.setText(Double.toString(location.getLongitude()));
-		
-		(new GetAddressTask(this)).execute(location);
-		
-		user.setText(getIntent().getStringExtra("USERNAME"));
-		
-	}
-	
-	private class AlertPlayer extends AsyncTask<Void, Void, Void>
-	{
 
-		@Override
-		protected Void doInBackground(Void... params) {
-			// TODO Auto-generated method stub
-			
-			alertClicked=!alertClicked;
-			Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-			RingtoneManager.getRingtone(getApplicationContext(), notification);
-			MediaPlayer mp = MediaPlayer.create(AddLocationActivity.this, R.raw.siren);
-			
-			AudioManager audioManager =
-					(AudioManager)AddLocationActivity.this.getSystemService(Context.AUDIO_SERVICE);
-					// Set the volume of played media to maximum.
-					audioManager.setStreamVolume (
-					AudioManager.STREAM_MUSIC,
-					audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
-					0);
-					
-			mp.setOnCompletionListener(new OnCompletionListener() {
-				
-				@Override
-				public void onCompletion(MediaPlayer mp) {
-					// TODO Auto-generated method stub
-					if(alertClicked && brojac<4)
-					{
-						brojac++;
-						mp.start();
-					}
-					else
-						if(brojac==4 && alertClicked)
-						{
-							brojac=0;
-							Toast.makeText(AddLocationActivity.this, "Proslo 16sec", Toast.LENGTH_LONG).show();
-			                mp.release();
-			               
-						}
-					
-				}
-			});
-		
-			if(alertClicked)
-			{
-				brojac++;
-				mp.start();	
-			}
-			else
-				mp.stop();
-			
-			return null;
-		}
-		
-		
-		
+		(new GetAddressTask(this)).execute(location);
+
+		user.setText(getIntent().getStringExtra("USERNAME"));
+
 	}
-	
+
 	
 	@Override
 	public void onClick(View v)
@@ -185,21 +121,6 @@ public class AddLocationActivity extends Activity implements OnClickListener {
 		// TODO Auto-generated method stub
 		switch(v.getId())
 		{
-		case R.id.btn_take_picture:
-			argss[0] = user.getText().toString();
-			
-			String fileName = argss[0];
-			ContentValues values = new ContentValues();
-			values.put(MediaStore.Images.Media.TITLE, fileName);
-			values.put(MediaStore.Images.Media.DESCRIPTION,"Image capture by camera");
-			selectedImage = getContentResolver().insert(
-			        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-			Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-			intent.putExtra(MediaStore.EXTRA_OUTPUT, selectedImage);
-
-			startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-
-			   break;
 		case R.id.btn_save_location:
 			
 			if(category!="" && description.getText().toString()!="")
@@ -224,29 +145,6 @@ public class AddLocationActivity extends Activity implements OnClickListener {
 		}
 		
 	}
-	
-	protected void onActivityResult(int requestCode, int resultCode, Intent data)
-	{
-
-		ImageView  img = (ImageView) findViewById(R.id.img_location);
-
-        photo_dest= getRealPathFromURI(selectedImage);
-        
-        img.setImageURI(selectedImage);
-        
-	}
-
-
-	public String getRealPathFromURI(Uri contentUri) {
-	    String [] proj={MediaColumns.DATA};
-	    android.database.Cursor cursor = managedQuery( contentUri,proj,null, null, null);     // Order-by clause (ascending by name)
-	    int column_index = cursor.getColumnIndexOrThrow(MediaColumns.DATA);
-	    cursor.moveToFirst();
-	    return cursor.getString(column_index);
-	    
-	}
-	
-
 	
 	public void onRadioButtonClicked(View view) {
 	    // Is the button now checked?
@@ -411,122 +309,10 @@ public class AddLocationActivity extends Activity implements OnClickListener {
 	         * After completing background task Dismiss the progress dialog
 	         * **/
 	        protected void onPostExecute(String file_url) {
-	        	
-	        	dialog = ProgressDialog.show(AddLocationActivity.this, "", "Uploading information of location...", true);
-	        	new Thread(new Runnable() {
-	                public void run() {
-	                     runOnUiThread(new Runnable() {
-	                            public void run() {
-	                                
-	                            }
-	                        });                     
-	                    uploadFile(photo_dest);                   
-	                }
-	              }).start(); 
-				
-	        	runOnUiThread(new Runnable() {
-	                public void run() {
-	                    /**
-	                     * Updating parsed JSON data into ListView
-	                     * */
-	                	if( success == 0 ) 
-	                		Toast.makeText(AddLocationActivity.this, "GRESKA NEKA - asinhroni task!" , Toast.LENGTH_LONG).show();
-	                	else
-	                		Toast.makeText(AddLocationActivity.this, "Proslo je - addlocation activity asinhroni task" , Toast.LENGTH_LONG).show();
-	                	
-	                }
-	            });
+
 	        	
 	        }
 	 
 	    }
-		
-		
-		
-		
-		 public void uploadFile(String sourceFileUri) {
-	         String upLoadServerUri = "http://nikolamilica10.site90.com/upload_photo_of_marker.php";
-	         String fileName = sourceFileUri;
-
-	         HttpURLConnection conn = null;
-	         DataOutputStream dos = null; 
-	         String lineEnd = "\r\n";
-	         String twoHyphens = "--";
-	         String boundary = "*****";
-	         int bytesRead, bytesAvailable, bufferSize;
-	         byte[] buffer;
-	         int maxBufferSize = 1 * 1024 * 1024;
-	         File sourceFile = new File(sourceFileUri);
-	         if (!sourceFile.isFile()) {
-	          Log.e("uploadFile", "Source File Does not exist");
-	         }
-	             try { // open a URL connection to the Servlet
-	              FileInputStream fileInputStream = new FileInputStream(sourceFile);
-	              URL url = new URL(upLoadServerUri);
-	              conn = (HttpURLConnection) url.openConnection(); // Open a HTTP  connection to  the URL
-	              conn.setDoInput(true); // Allow Inputs
-	              conn.setDoOutput(true); // Allow Outputs
-	              conn.setUseCaches(false); // Don't use a Cached Copy
-	              conn.setRequestMethod("POST");
-	              conn.setRequestProperty("Connection", "Keep-Alive");
-	              conn.setRequestProperty("ENCTYPE", "multipart/form-data");
-	              conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-	              conn.setRequestProperty("uploaded_file", fileName);
-	              dos = new DataOutputStream(conn.getOutputStream());
-	    
-	              dos.writeBytes(twoHyphens + boundary + lineEnd);
-	              dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""+ img_marker_id+".jpg" + "\"" + lineEnd);
-	              dos.writeBytes(lineEnd);
-	    
-	              bytesAvailable = fileInputStream.available(); // create a buffer of  maximum size
-	    
-	              bufferSize = Math.min(bytesAvailable, maxBufferSize);
-	              buffer = new byte[bufferSize];
-	    
-	              // read file and write it into form...
-	              bytesRead = fileInputStream.read(buffer, 0, bufferSize); 
-	                
-	              while (bytesRead > 0) {
-	                dos.write(buffer, 0, bufferSize);
-	                bytesAvailable = fileInputStream.available();
-	                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-	                bytesRead = fileInputStream.read(buffer, 0, bufferSize);              
-	               }
-	    
-	              // send multipart form data necesssary after file data...
-	              dos.writeBytes(lineEnd);
-	              dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-	    
-	              // Responses from the server (code and message)
-	              serverResponseCode = conn.getResponseCode();
-	              String serverResponseMessage = conn.getResponseMessage();
-	               
-	              Log.i("uploadFile", "HTTP Response is : " + serverResponseMessage + ": " + serverResponseCode);
-	              if(serverResponseCode == 200){
-	                  runOnUiThread(new Runnable() {
-	                       public void run() {
-	                           Toast.makeText(AddLocationActivity.this, "Successfully upload information of location!", Toast.LENGTH_SHORT).show();
-	                       }
-	                   });               
-	              }   
-	              
-	              //close the streams //
-	              fileInputStream.close();
-	              dos.flush();
-	              dos.close();
-	               
-	         } catch (MalformedURLException ex) { 
-	             dialog.dismiss(); 
-	             ex.printStackTrace();
-	             Toast.makeText(AddLocationActivity.this, "MalformedURLException", Toast.LENGTH_SHORT).show();
-	             Log.e("Upload file to server", "error: " + ex.getMessage(), ex); 
-	         } catch (Exception e) {
-	             dialog.dismiss(); 
-	             e.printStackTrace();
-	             Toast.makeText(AddLocationActivity.this, "Exception : " + e.getMessage(), Toast.LENGTH_SHORT).show();
-	             Log.e("Upload file to server Exception", "Exception : " + e.getMessage(), e); 
-	         }
-	         dialog.dismiss();      
-	        }
 	
 }
